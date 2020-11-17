ba4687241ff1a9e77366a5d870b15a28dad3b1ab
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-11-17 15:20:46 +0000

    Add files via upload

---
+++ GUI_IO/GUI_IOTester.java
@@ -0,0 +1,22 @@
+
+public class GUI_IOTester {
+
+	public static void main(String[] args) {
+	
+
+		
+		
+	
+		 GUI_IO gui1;
+		 gui1 = new GUI_IO();
+	
+		gui1.basicMessage();
+		gui1.titleBarWarning();
+		gui1.basicPromptDisplay();
+		
+		
+		
+		
+	}
+
+}
