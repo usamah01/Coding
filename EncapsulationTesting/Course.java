1d17ed35a4c3384a618fa283622f8beff10d09c4
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-11-17 15:21:19 +0000

    Add files via upload

---
+++ EncapsulationTesting/Course.java
@@ -0,0 +1,17 @@
+
+public class Course
+{  
+   public static void main (String args[])
+   {
+	   Student s=new Student("12345","Michael");
+	   
+	  
+	   System.out.println(s.name);
+	   
+	
+	   System.out.println(s.getName());
+   }
+
+}
+    
+   
---
+++ EncapsulationTesting/Student.java
@@ -0,0 +1,29 @@
+
+public class Student
+{
+   
+    private String id;
+    public String name;
+
+  
+    public Student(String id,String name)
+    {
+        
+        this.id = id;
+        this.name=name;
+    }
+
+   
+    public String getid()
+    {
+      
+        return id;
+    }
+    
+    public String getName()
+    {
+       
+        return name;
+    }
+    
+}
