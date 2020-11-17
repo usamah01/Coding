f0789f991416f0b83b1ff554ab6df06e3b51c58a
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-11-17 15:23:03 +0000

    Add files via upload

---
+++ forLoop/ForLoopTest.java
@@ -0,0 +1,21 @@
+
+public class ForLoopTest
+{  
+    public void displayNumbers()
+    {
+        
+        for (int i=9;i>=0;i--)
+        {
+            System.out.println("the value of i is "  + i);
+        }
+    }
+    
+    
+    public static void main(String args[])
+    {
+    	
+        ForLoopTest f=new ForLoopTest();
+    	f.displayNumbers();
+    	
+    }
+}
