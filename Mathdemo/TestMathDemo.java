234a81d948c472181d4e365a98566861a846365b
Usamah Ahmed <62649176+usamah01@users.noreply.github.com>
2020-11-17 15:24:45 +0000

    Add files via upload

---
+++ Mathdemo/TestMathDemo.java
@@ -0,0 +1,13 @@
+
+public class TestMathDemo {
+
+	public static void main(String[] args) {
+                        
+MathDemo m = new MathDemo();
+m.squareRoot(3);
+m.rollDice();
+m.findThePower();
+			
+	}
+
+}
